a6839b024c8fd9b2e5504d806d79db72d35b0a51
Tim Dallau <tim@dallau.com>
2018-03-13 11:33:05 +0100

    Chart.js

--- src/app/_services/common.service.ts
+++ src/app/_services/common.service.ts
@@ -1,16 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CommonService {
 
-  url = "http://localhost:5000/"
+  url = 'http://localhost:5000/';
 
   constructor(private http: HttpClient) { }
 
-  getData(): Observable<any> {
-    return this.http.get<any>(this.url + "api/crime");
+  getCrime(crime_end_type = 'Geregistreerde misdrijven') {
+    return this.http.post(this.url + 'api/crime', { 'crime_end_type' : crime_end_type})
+      .map(result => result);
   }
+
+  getWork(wvtName = 'Banen') {
+    return this.http.post(this.url + 'api/work', { 'wvtName' : wvtName})
+      .map(result => result);
+  }
+
+  getEducation(gender = 'Totaal mannen en vrouwen') {
+    return this.http.post(this.url + 'api/education', { 'gender' : gender})
+      .map(result => result);
+  }
+
+  merge_array(array1, array2) {
+    const result_array = [];
+    const arr = array1.concat(array2);
+    let len = arr.length;
+    const assoc = {};
+
+    while (len--) {
+      const item = arr[len];
+
+        if (!assoc[item]) {
+            result_array.unshift(item);
+            assoc[item] = true;
+        }
+    }
+
+    return result_array;
+}
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, EventEmitter} from '@angular/core';
 import { CommonService } from '../_services/common.service';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +9,145 @@ import { CommonService } from '../_services/common.service';
 })
 export class HomeComponent implements OnInit {
 
-  private data;
+  makeChart = new EventEmitter<any>();
+
+  private crime = {
+    data : [],
+    label : 'crime',
+    color : 'rgba(255, 99, 132, 0.2)',
+    horizontalLabel : []
+  };
+  private work = {
+    data : [],
+    label : 'work(x10)',
+    color : 'rgba(54, 162, 235, 0.2)',
+    horizontalLabel : []
+  };
+  private education = {
+    data : [],
+    label : 'education(x10)',
+    color : 'rgba(255, 206, 86, 0.2)',
+    horizontalLabel : []
+  };
+
+  private datasets = [
+    {
+      data : this.crime.data,
+      label : this.crime.label,
+      backgroundColor : this.crime.color,
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1
+    },
+    {
+      data : this.work.data,
+      label : this.work.label,
+      backgroundColor : this.work.color,
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    },
+    {
+      data : this.education.data,
+      label : this.education.label,
+      backgroundColor : this.education.color,
+      borderColor: 'rgba(255, 206, 86, 1)',
+      borderWidth: 1
+    }
+  ];
+
+  public chart = [];
+  public devide = 1;
 
   constructor(private commonService: CommonService) { }
 
   ngOnInit() {
-    this.test()
+    this.makeChart.subscribe(data => {
+      const labels = this.commonService.merge_array(this.education.horizontalLabel, this.work.horizontalLabel);
+      this.chart = new Chart('canvas', {
+        type: 'line',
+        data : {
+          labels : labels,
+          datasets : this.datasets
+        },
+        options: {
+          scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+        }
+      });
+    });
+    this.getCrime();
+    this.getWork();
+    this.getEducation();
   }
 
-  test(): void {
-    this.commonService.getData()
-        .subscribe(
-          data =>  this.data = data
-        );
+  private createChart() {
+    this.makeChart.emit();
+  }
+
+  public getCrime(filter = 'Geregistreerde misdrijven') {
+    // console.log(filter);
+    this.commonService.getCrime(filter).subscribe((data: [any]) => {
+      this.crime.data = data.map(res => res.amount);
+      // this.crime.data.unshift(null, null, null, null);
+      this.crime.horizontalLabel = data.map(res => res.year);
+
+      const newLabel = 'Crime' + ' (' + filter + ')';
+
+      this.updateDataSets(this.crime.label, this.crime.data, this.crime.horizontalLabel, newLabel);
+      this.crime.label = newLabel;
+      this.createChart();
+    });
+  }
+
+  public getWork(filter = 'Banen') {
+    this.commonService.getWork(filter).subscribe((data: [any]) => {
+      this.work.data = data.map(res => res.amount / this.devide);
+      // this.work.data.unshift(null, null, null, null, null);
+      this.work.horizontalLabel = data.map(res => res.year);
+
+      const newLabel = 'Work (x' + this.devide + ')' + ' (' + filter + ')';
+
+      this.updateDataSets(this.work.label, this.work.data, this.work.horizontalLabel, newLabel);
+      this.work.label = newLabel;
+      this.createChart();
+    });
+  }
+
+  public getEducation(filter = 'Totaal mannen en vrouwen') {
+    this.commonService.getEducation(filter).subscribe((data: [any]) => {
+      this.education.data = data.map(res => res.amount / this.devide);
+      this.education.horizontalLabel = data.map(res => res.year);
+
+      const newLabel = 'Education (x' + this.devide + ')' + ' (' + filter + ')';
+
+      this.updateDataSets(this.education.label, this.education.data, this.education.horizontalLabel, newLabel );
+      this.education.label = newLabel;
+      this.createChart();
+    });
+  }
+
+  public updateDataSets (label, data, horizontalLabel, newLabel) {
+    this.datasets.forEach((element, key) => {
+      if (element.label === label) {
+
+        this.datasets[key].data = data;
+        this.datasets[key].label = newLabel;
+      }
+    });
+  }
+
+  public updateDevide() {
+    if (this.devide === 10) {
+      this.devide = 1;
+    } else {
+      this.devide = 10;
+    }
+    this.getEducation();
+    this.getWork();
   }
 
 }
